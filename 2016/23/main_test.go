58464c17fa7f55b6615a7aff650642c6c0c1178c
Chris Walker <chris@thechriswalker.net>
2018-12-11 09:12:03 +0000

    Add 2016/23

---
+++ 2016/23/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+)
+
+// tests for the AdventOfCode 2016 day 23 solutions
+
+type Case struct {
+	In  string
+	Out string
+}
+
+var problem1cases = []Case{
+	// cases here
+	{`cpy 2 a
+	tgl a
+	tgl a
+	tgl a
+	cpy 1 a
+	dec a
+	dec a`, "3"},
+}
+
+func TestProblem1(t *testing.T) {
+	for _, c := range problem1cases {
+		actual := solve1(c.In)
+		if c.Out != actual {
+			t.Fatalf("Expected: '%s', Actual: '%s'", c.Out, actual)
+		}
+	}
+}
+
+var problem2cases = []Case{
+	// cases here
+	//	{"", ""},
+}
+
+func TestProblem2(t *testing.T) {
+	for _, c := range problem2cases {
+		actual := solve2(c.In)
+		if c.Out != actual {
+			t.Fatalf("Expected: '%s', Actual: '%s'", c.Out, actual)
+		}
+	}
+}
