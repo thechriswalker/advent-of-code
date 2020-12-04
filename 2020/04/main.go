e2c81c84ababb2ff07102bc96e5964951e9d1e3d
Chris Walker <chris@thechriswalker.net>
2020-12-04 11:03:34 +0000

    Day 4, 2020

---
+++ 2020/04/main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"../../aoc"
+)
+
+func main() {
+	aoc.Run(2020, 4, solve1, solve2)
+}
+
+type passport map[string]string
+
+func (p passport) HasFields(required []string) bool {
+	for _, s := range required {
+		if _, ok := p[s]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (p passport) HasValidFields(v map[string]func(string) bool) bool {
+	for key, fn := range v {
+		val, ok := p[key]
+		if !ok || !fn(val) {
+			return false
+		}
+	}
+	return true
+}
+
+func parsePassports(input string) []passport {
+	stack := make([]passport, 0, 500) // I don't know how many there will be...
+	// we kinda want to iterate over the file line by line.
+	lines := strings.Split(input, "\n")
+	current := make(passport)
+	for _, line := range lines {
+		if line == "" {
+			stack = append(stack, current)
+			current = make(passport)
+		}
+		for _, kv := range strings.Split(line, " ") {
+			x := strings.Split(kv, ":")
+			if len(x) == 2 {
+				current[x[0]] = x[1]
+			}
+		}
+	}
+	if len(current) != 0 {
+		stack = append(stack, current)
+	}
+
+	return stack
+}
+
+// Implement Solution to Problem 1
+func solve1(input string) string {
+	passports := parsePassports(input)
+	sum := 0
+	req := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	for _, p := range passports {
+		if p.HasFields(req) {
+			sum++
+		}
+	}
+	return fmt.Sprintf("%d", sum)
+}
+
+// Implement Solution to Problem 2
+func solve2(input string) string {
+	passports := parsePassports(input)
+	sum := 0
+	for _, p := range passports {
+		if p.HasValidFields(validators) {
+			sum++
+		}
+	}
+	return fmt.Sprintf("%d", sum)
+	return "<unsolved>"
+}
+
+var validators = map[string]func(string) bool{
+	"byr": func(s string) bool {
+		if len(s) != 4 {
+			return false
+		}
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return false
+		}
+		return i >= 1920 && i <= 2002
+	},
+	"iyr": func(s string) bool {
+		if len(s) != 4 {
+			return false
+		}
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return false
+		}
+		return i >= 2010 && i <= 2020
+	},
+	"eyr": func(s string) bool {
+		if len(s) != 4 {
+			return false
+		}
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return false
+		}
+		return i >= 2020 && i <= 2030
+	},
+	"hgt": func(s string) bool {
+		if len(s) < 3 {
+			return false
+		}
+		u := s[len(s)-2:]
+		v, err := strconv.Atoi(s[:len(s)-2])
+		if err != nil {
+			return false
+		}
+		switch u {
+		case "cm":
+			return v >= 150 && v <= 193
+		case "in":
+			return v >= 59 && v <= 76
+		default:
+			return false
+		}
+	},
+	"hcl": func(s string) bool {
+		if len(s) != 7 {
+			return false
+		}
+		if s[0] != '#' {
+			return false
+		}
+		for _, c := range s[1:] {
+			if c >= 'a' || c <= 'f' {
+				continue
+			}
+			if c >= '0' || c <= '9' {
+				continue
+			}
+			return false
+		}
+		return true
+	},
+	"ecl": func(s string) bool {
+		switch s {
+		case "amb", "blu", "brn", "gry", "grn", "hzl", "oth":
+			return true
+		default:
+			return false
+		}
+	},
+	"pid": func(s string) bool {
+		if len(s) != 9 {
+			return false
+		}
+		for _, c := range s {
+			if c >= '0' || c <= '9' {
+				continue
+			}
+			return false
+		}
+		return true
+	},
+}
---
+++ 2020/04/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"testing"
+)
+
+// tests for the AdventOfCode 2020 day 4 solutions
+
+type Case struct {
+	In  string
+	Out string
+}
+
+var problem1cases = []Case{
+	// cases here
+	{`ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
+`, "2"},
+}
+
+func TestProblem1(t *testing.T) {
+	for _, c := range problem1cases {
+		actual := solve1(c.In)
+		if c.Out != actual {
+			t.Fatalf("Expected: '%s', Actual: '%s'", c.Out, actual)
+		}
+	}
+}
+
+var problem2cases = []Case{
+	// cases here
+	{`eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
+`, "0"},
+	{`pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+`, "4"},
+}
+
+func TestProblem2(t *testing.T) {
+	for _, c := range problem2cases {
+		actual := solve2(c.In)
+		if c.Out != actual {
+			t.Fatalf("Expected: '%s', Actual: '%s'", c.Out, actual)
+		}
+	}
+}
